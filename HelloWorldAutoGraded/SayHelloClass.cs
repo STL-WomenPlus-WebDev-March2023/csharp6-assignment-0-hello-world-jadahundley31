bc8908b97e5869ac8efc7992c8a1f6c2c4418df4
jadahundley31 <jadahundley@gmail.com>
2023-08-19 21:12:43 -0500

    changed &&& to Hello, World!

--- HelloWorldAutoGraded/SayHelloClass.cs
+++ HelloWorldAutoGraded/SayHelloClass.cs
@@ -2,18 +2,44 @@
 namespace HelloWorldAutoGraded
 {
 	public class SayHelloClass
-	{
-		//Update the method to pass the auto-graded test
+    {
+        //Update the method to pass the auto-graded test
 
 		public static string SayHello()
 		{
-			return "&&&";
+			return "Hello, World!";
 		}
 
         //class constructor - intentionally empty
         public SayHelloClass()
         {
 
+        }
+    }
+
+    internal record struct NewStruct(object Item1, object Item2)
+    {
+        public static implicit operator (object, object)(NewStruct value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct((object, object) value)
+        {
+            return new NewStruct(value.Item1, value.Item2);
+        }
+    }
+
+    internal record struct NewStruct1(object Item1, object Item2)
+    {
+        public static implicit operator (object, object)(NewStruct1 value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct1((object, object) value)
+        {
+            return new NewStruct1(value.Item1, value.Item2);
         }
     }
 }
